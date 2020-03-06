88bff50ae7162ac147c6ea8a75f0ecd5547b48a3
Chandra Asar <chandra_asar@anfcorp.com>
2020-03-06 16:05:28 -0500

    Updates

---
+++ precache-manifest.bd4039f127880dad799f7320ede3b83b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f11738f57411d4190447ee1009fcad",
+    "url": "/ReactPractice-MonstersRolodex/index.html"
+  },
+  {
+    "revision": "630edf391cf3dc70b7f7",
+    "url": "/ReactPractice-MonstersRolodex/static/css/main.2fd677f5.chunk.css"
+  },
+  {
+    "revision": "f89ca1db1bd198bc85cb",
+    "url": "/ReactPractice-MonstersRolodex/static/js/2.85953fe7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactPractice-MonstersRolodex/static/js/2.85953fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630edf391cf3dc70b7f7",
+    "url": "/ReactPractice-MonstersRolodex/static/js/main.ace3ba94.chunk.js"
+  },
+  {
+    "revision": "73dd11cb89c3753c0b13",
+    "url": "/ReactPractice-MonstersRolodex/static/js/runtime-main.566d3293.js"
+  }
+]);
